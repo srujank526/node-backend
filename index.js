c6068b88ae08352058049f791d1371240d7a76bb
srujank526 <srujank526@gmail.com>
2024-07-20 21:57:06 +0530

    adding socket

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import  express from 'express';
 import http from 'http';
 import cors from 'cors'
+import { Server } from "socket.io";
+
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.use(cors());
 
 app.get("/",(req,res)=>{
@@ -13,3 +16,6 @@ app.get("/",(req,res)=>{
 server.listen(PORT, () => {
 	console.log(`The server listening at port ${PORT}`);
 });
+io.on('connection',(socket)=>{
+    console.log(socket)
+})
