0b1f7b0557862ace9dbfc51c3eb078f8aae5811b
srujank526 <srujank526@gmail.com>
2024-07-20 21:04:54 +0530

    third commit

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import  express from 'express';
 import http from 'http';
+import cors from 'cors'
 const app = express();
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 3000;
+app.use(cors());
 
 app.get("/",(req,res)=>{
     res.send({message:"hello srujan"})
